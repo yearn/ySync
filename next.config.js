c6cabbda8623cef86a9468d1b0148d8d82d445e2
Majorfi <major-tom.eth@pm.me>
2022-06-08 12:23:33 +0200

    feat: prepare project transfer

--- next.config.js
+++ next.config.js
@@ -16,11 +16,11 @@ module.exports = ({
 		** Stuff used for the SEO or some related elements, like the title, the
 		** github url etc.
 		**********************************************************************/
-		WEBSITE_URI: 'https://sync.ycorpo.com/',
-		WEBSITE_NAME: 'yData Sync',
-		WEBSITE_TITLE: 'yData Sync',
+		WEBSITE_URI: 'https://sync.yearn.farm/',
+		WEBSITE_NAME: 'ySync',
+		WEBSITE_TITLE: 'ySync',
 		WEBSITE_DESCRIPTION: 'Sync data from Yearn\'s data sources',
-		PROJECT_GITHUB_URL: 'https://github.com/yearn/yearn-template',
+		PROJECT_GITHUB_URL: 'https://github.com/yearn/yearn-data-sync',
 
 		/* 🔵 - Yearn Finance **************************************************
 		** Some config used to control the behaviour of the web library. By
@@ -38,13 +38,6 @@ module.exports = ({
 		USE_PRICE_TRI_CRYPTO: false,
 		CG_IDS: [],
 		TOKENS: [],
-		RPC_URL: {
-			1: process.env.RPC_URL_MAINNET,
-			250: process.env.RPC_URL_FANTOM || 'https://rpc.ftm.tools',
-			42161: process.env.RPC_URL_ARBITRUM || 'https://arbitrum.public-rpc.com'
-		},
-		ALCHEMY_KEY: process.env.ALCHEMY_KEY,
-		META_API_URL: 'https://meta.yearn.finance/api',
-		META_GITHUB_URL: 'https://github.com/yearn/yearn-meta'
+		ALCHEMY_KEY: process.env.ALCHEMY_KEY
 	}
 });
